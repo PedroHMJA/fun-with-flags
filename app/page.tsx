c09b1f8bf7f47bf1f38c29717099397ac01c3bfc
PedroHMJA <pedromayer.alves@outlook.com>
2024-12-26 02:16:33 -0300

    Create the card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import { Header, Footer } from './components';
+import { Header, Footer, Card } from './components';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex-1">This is the main</main>
+      <main className="flex-1">
+        <Card 
+          country="Brazil"
+          capital="Brasilia"
+          region="South America"
+          population="216000000"
+        />
+      </main>
       <Footer />
     </>
   );
