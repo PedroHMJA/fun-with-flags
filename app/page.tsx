74f8fb8120e44c48ddc6016326539d01201d4cc9
PedroHMJA <pedromayer.alves@outlook.com>
2024-12-30 01:58:46 -0300

    Implementando uma chamada à API restcountries.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,32 @@
+"use client";
+import { useEffect, useState } from 'react';
 import { Header, Footer, Card, Grid } from './components';
 
 
-const countries = [
-  {
-    id: "1",
-    country: "Brazil",
-    capital: "Brasilia",
-    region: "South America",
-    population: "214000000",
-  }
-  ];
-
 
 export default function Home() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      setCountries(data);
+    };
+    fetchCountries();
+  }, []);
+
+
   return (
     <>
       <Header />
       <main className="flex-1">
       <Grid>
-          {countries.map(({id, country, capital, region, population}) => (
+          {countries.map(({id, name, capital, region, population}) => (
           <Card
           key={id}
-          country={country}
+          country={name.common}
           capital={capital}
           region={region}
           population={population}
