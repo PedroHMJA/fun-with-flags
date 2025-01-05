d163681c360c59a93d59bb5b6c9c1b2cd048b234
PedroHMJA <pedromayer.alves@outlook.com>
2025-01-05 00:18:03 -0300

    get countries data from rest countries API

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,68 @@ import { Header, Footer, Card, Grid } from './components';
 
 export default function Home() {
 
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState<Country[]>([]);
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-      const response = await fetch("https://restcountries.com/v3.1/all");
-      const data = await response.json();
-      setCountries(data);
+  const [loading, setLoading]  = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  type Country = {
+    cca3: string;
+    flags: {
+      svg: string;
+    },
+    name: {
+      common:string;
     };
-    fetchCountries();
-  }, []);
+    capital: string[];
+    region:string;
+    population: number;
+  };
 
+    useEffect(() => {
+      const fetchCountries = async () => {
+        try 
+        {
+          const response = await fetch("https://restcountries.com/v3.1/all?fields=cca3,flags,name,capital,region,population");
+          const data = await response.json();
+          setCountries(data);
+        } catch (error) {
+          setError("Failed to fetch data");
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+
+      };
+      fetchCountries();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
 
   return (
     <>
       <Header />
       <main className="flex-1">
       <Grid>
-          {countries.map(({id, name, capital, region, population}) => (
+          {countries.map(({cca3, flags, name, capital, region, population}, index) => {
+          const { svg:flag } = flags ?? {};
+          const { common:countryName } = name ?? {};
+          const [capitalName] = capital ?? [];
+          
+          return ( 
           <Card
-          key={id}
-          country={name.common}
-          capital={capital}
+          key={cca3}
+          index={index}
+          flag={flag}
+          name={countryName}
+          capital={capitalName}
           region={region}
           population={population}
-        />))}
+        />
+      );
+    }
+      )}
       </Grid>
       </main>
       <Footer />
